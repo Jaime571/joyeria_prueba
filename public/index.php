d503933281ce93a6e1a11fc41660361accd69642
jaime <lockdown97140@gmail.com>
2025-06-30 22:18:36 -0600

    test

--- public/index.php
+++ public/index.php
@@ -61,8 +61,8 @@
         <hr>
         <!-- Deck de cartas de los clientes -->
         <?php
-        include_once 'src/config/db_connection.php';
-        include_once 'src/config/queries.php';
+        include_once '../src/config/db_connection.php';
+        include_once '../src/config/queries.php';
 
         // Preparar la consulta
         $clientes = $db->prepare($getAllClients);
