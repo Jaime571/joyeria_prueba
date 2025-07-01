eaf8c006dd55d50e24ce3fba7c0362c100e54235
jaime <lockdown97140@gmail.com>
2025-06-30 22:16:15 -0600

    minor changes on index

--- public/index.php
+++ public/index.php
@@ -61,8 +61,8 @@
         <hr>
         <!-- Deck de cartas de los clientes -->
         <?php
-        include_once './db/db_connection.php';
-        include_once './db/queries.php';
+        include_once 'src/config/db_connection.php';
+        include_once 'src/config/queries.php';
 
         // Preparar la consulta
         $clientes = $db->prepare($getAllClients);
